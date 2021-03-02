234dc397891fd4d8fe6dfd936cf65063034e023c
ItzelVigil <itzel.vigilch@gmail.com>
2021-03-02 00:16:51 -0700

    3

--- desktop.py
+++ desktop.py
@@ -22,5 +22,30 @@ file = file.fillna(0)
 g = file.head(20)
 st.write(g)
 
+import fiona
+fiona.drvsupport.supported_drivers['libkml'] = 'rw'
+fiona.drvsupport.supported_drivers['LIBKML'] = 'rw'
+gdl = gpd.read_file("Dashboard Estadística/Files/Poly/DISTRITOS_SONORA.kml")
+gdl = gdl[["Name", "geometry"]]
+gdl = gdl.sort_values("Name").reset_index(drop=True)
+gdl["Porcentaje"] = 0.0
+
+fig, ax = plt.subplots(1, 1,figsize=(20,12))
+
+divider = make_axes_locatable(ax)
+
+#cax = divider.append_axes("right", size="5%", pad=0.1)
+
+gdl.plot(figsize=(20, 20),
+         edgecolor = "black",
+         column="Porcentaje",
+         ax=ax,
+         legend=True,
+         cmap="Spectral",
+         vmax=100,
+         vmin=0,
+         legend_kwds={'label': "Porcentaje",
+                      'orientation': "vertical"})
+st.pyplot()
 
 
